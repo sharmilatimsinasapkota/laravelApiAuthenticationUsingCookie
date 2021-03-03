2bf292e0755a2a756aed513272037a43137a432f
Sharmila Timsina Sapkota <sharmila.sapkota@splicecom.com>
2021-03-03 15:17:22 +0000

    Initial Commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
+use Symfony\Component\HttpFoundation\Response;
+//use this when getting the stored cookie for logout section
+use Illuminate\Support\Facades\Cookie;
+
+class AuthController extends Controller
+{
+    public function hello()
+    {
+        return 'Hello from controller';
+    }
+    public function register(RegisterRequest $request)
+    {
+        $user=User::create([
+            'first_name'=>$request->input('first_name'),
+            'last_name'=>$request->input('last_name'),
+            'email'=>$request->input('email'),
+            'password'=>Hash::make($request->input('password')),
+        ]);
+        return response($user,Response::HTTP_CREATED);
+    }
+    
+    public function login(Request $request)
+    {
+
+        if(!Auth::attempt($request->only('email','password'))){
+            return \response([
+                'error'=>'Invalid Credentials!'
+            ],Response::HTTP_UNAUTHORIZED);
+        }
+        $user=Auth::user();
+        $token=$user->createToken('token')->plainTextToken;
+//extra code for cookie
+        $cookie=cookie('jwt',$token,1440); //valid for one day
+        return \response([
+            'jwt'=>$token
+        ])->withCookie($cookie);
+
+//extra code ends here
+/*
+        return \response([
+            'jwt'=>$token
+        ]);*/
+    }
+
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
+  
+    public function logout()
+    {
+        $cookie=Cookie::forget('jwt');
+        return \response([
+            'message'=>'success'
+        ])->withCookie($cookie);
+    }
+
+
+}
---
+++ resources/views/reset.blade.php
@@ -0,0 +1,1 @@
+Click <a href="http://127.0.0.1:8006/reset/{{ $token }}">here</a> to reset your password 
